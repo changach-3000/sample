8daf1ba758caee31122ca2559798819829498973
Chang'ach <126554394+changach-3000@users.noreply.github.com>
2024-06-18 09:55:13 +0300

    create initial files for layout and index file

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+
+   <View className="flex-1 items-center justify-center bg-white">
+      <Text>Hello, You!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
